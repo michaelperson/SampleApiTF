51c9cae02c02c262ff1c7e3646c645f67e2ade1e
michael person <michael.person@cognitic.be>
2023-07-11 16:51:31 +0200

    Add Student ef

--- SampleApi.Repositories/StudentRepository.cs
+++ SampleApi.Repositories/StudentRepository.cs
@@ -17,39 +17,57 @@ namespace SampleApi.Repositories
 			_cnstr = cnstr;
 		}
 
-		public int Add(StudentPOCO obj)
+		#region ADO
+		//public int Add(StudentPOCO obj)
+		//{
+		//	int id;
+		//	try
+		//	{
+		//		using (SqlConnection conn = new SqlConnection(_cnstr))
+		//		{
+		//			conn.Open();
+		//			using (SqlCommand cmd = conn.CreateCommand())
+		//			{
+		//				cmd.CommandText = "INSERT INTO STUDENT OUTPUT inserted.student_id VALUES ( @firstname, @lastname, @birthdate, @login, @sectionid, @yearresult, @courseid)";
+
+		//				cmd.Parameters.Add("@firstname", System.Data.SqlDbType.VarChar).Value = obj.First_Name;
+		//				cmd.Parameters.AddWithValue("lastname", obj.Last_Name);
+		//				cmd.Parameters.AddWithValue("birthdate", obj.BirthDate);
+		//				cmd.Parameters.AddWithValue("login", obj.Login);
+		//				cmd.Parameters.AddWithValue("sectionid", obj.Section_ID);
+		//				cmd.Parameters.AddWithValue("yearresult", obj.Year_Result);
+		//				cmd.Parameters.AddWithValue("courseid", obj.Course_ID);
+
+		//			 id=(int)cmd.ExecuteScalar();
+		//			}
+		//			conn.Close();
+		//			return id;
+		//		}
+		//	}
+		//	catch (Exception ex)
+		//	{
+		//		//TODO : manage exception
+		//		throw;
+		//	}
+		//} 
+		#endregion
+		public int Add(StudentPOCO studentPOCO)
 		{
-			int id;
+			DbSlideContext ctx = new DbSlideContext();
+			Student s = Map(studentPOCO);
+			ctx.Students.Add(s);
+
 			try
 			{
-				using (SqlConnection conn = new SqlConnection(_cnstr))
-				{
-					conn.Open();
-					using (SqlCommand cmd = conn.CreateCommand())
-					{
-						cmd.CommandText = "INSERT INTO STUDENT OUTPUT inserted.student_id VALUES ( @firstname, @lastname, @birthdate, @login, @sectionid, @yearresult, @courseid)";
-
-						cmd.Parameters.Add("@firstname", System.Data.SqlDbType.VarChar).Value = obj.First_Name;
-						cmd.Parameters.AddWithValue("lastname", obj.Last_Name);
-						cmd.Parameters.AddWithValue("birthdate", obj.BirthDate);
-						cmd.Parameters.AddWithValue("login", obj.Login);
-						cmd.Parameters.AddWithValue("sectionid", obj.Section_ID);
-						cmd.Parameters.AddWithValue("yearresult", obj.Year_Result);
-						cmd.Parameters.AddWithValue("courseid", obj.Course_ID);
-
-					 id=(int)cmd.ExecuteScalar();
-					}
-					conn.Close();
-					return id;
-				}
+				ctx.SaveChanges();
+				return (int)s.StudentId;
 			}
-			catch (Exception ex)
+			catch (Exception)
 			{
-				//TODO : manage exception
-				throw;
+				return -1;
 			}
+			 
 		}
-
 		public void Delete(int id)
 		{
 			throw new NotImplementedException();
@@ -72,24 +90,39 @@ namespace SampleApi.Repositories
 			return ctx.Students.Select(E=> StudentRepository.Map(E));
 		}
 
-		private static StudentPOCO Map(Student e)
-		{
-			return new StudentPOCO()
-			{
-				BirthDate = e.BirthDate.HasValue?e.BirthDate.Value:DateTime.MinValue,
-				Course_ID = e.CourseId,
-				First_Name = e.FirstName,
-				Last_Name = e.LastName,
-				Section_ID = e.SectionId.HasValue? e.SectionId.Value:0,
-				Student_ID = e.StudentId,
-				Year_Result = e.YearResult.HasValue?e.YearResult.Value:0
-
-			};
-		}
+		
 
 		public void Update(StudentPOCO obj)
 		{
 			throw new NotImplementedException();
+		}
+		private static Student Map(StudentPOCO e)
+		{
+			return new Student()
+			{
+				BirthDate = e.BirthDate,
+				CourseId =  e.Course_ID,
+				FirstName = e.First_Name,
+				LastName = e.Last_Name,
+				SectionId = e.Section_ID,
+				StudentId = e.Student_ID,
+				YearResult = e.Year_Result
+
+			};
+		}
+		private static StudentPOCO Map(Student e)
+		{
+			return new StudentPOCO()
+			{
+				BirthDate = e.BirthDate.HasValue ? e.BirthDate.Value : DateTime.MinValue,
+				Course_ID = e.CourseId,
+				First_Name = e.FirstName,
+				Last_Name = e.LastName,
+				Section_ID = e.SectionId.HasValue ? e.SectionId.Value : 0,
+				Student_ID = e.StudentId,
+				Year_Result = e.YearResult.HasValue ? e.YearResult.Value : 0
+
+			};
 		}
 	}
 }
